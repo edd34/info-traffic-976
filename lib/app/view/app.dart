6566d2187c7928096f699edf16f2d89bba3fd1c8
edd34 <eddine.farid@hotmail.fr>
2022-03-23 09:19:51 +0300

    implement Provider basic structure

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -8,27 +8,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:info_traffic_976/alert/alert.dart';
+import 'package:info_traffic_976/alert/providers/alert_providers.dart';
 import 'package:info_traffic_976/l10n/l10n.dart';
 import 'package:info_traffic_976/map/map.dart';
+import 'package:provider/provider.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
-        colorScheme: ColorScheme.fromSwatch(
-          accentColor: const Color(0xFF13B9FF),
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (context) => AlertProvider())],
+      child: MaterialApp(
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
+          colorScheme: ColorScheme.fromSwatch(
+            accentColor: const Color(0xFF13B9FF),
+          ),
         ),
+        localizationsDelegates: const [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+        ],
+        supportedLocales: AppLocalizations.supportedLocales,
+        home: const MyStatefulWidget(),
       ),
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-      ],
-      supportedLocales: AppLocalizations.supportedLocales,
-      home: const MyStatefulWidget(),
     );
   }
 }
