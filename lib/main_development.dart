8c695de0adc99757ad9e7d268556175b1d4af744
edd34 <eddine.farid@hotmail.fr>
2022-03-29 22:28:05 +0300

    remove use of boostrap code when running main

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -5,16 +5,14 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:info_traffic_976/app/app.dart';
-import 'package:info_traffic_976/bootstrap.dart';
 
-
-
-Future main() async {
+void main() {
   // To load the .env file contents into dotenv.
   // NOTE: fileName defaults to .env and can be omitted in this case.
   // Ensure that the filename corresponds to the path in step 1 and 2.
-  await dotenv.load();
-  await bootstrap(() => const App());
+  dotenv.load();
+  runApp(const App());
 }
