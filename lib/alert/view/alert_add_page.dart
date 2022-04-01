1ed96fdf32146ffa32d7a6fb85e26936a7259bce
edd34 <eddine.farid@hotmail.fr>
2022-04-01 20:36:00 +0300

    implement tab

--- lib/alert/view/alert_add_page.dart
+++ lib/alert/view/alert_add_page.dart
@@ -6,8 +6,26 @@ class AlertAddPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: GridLayout()),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.place)),
+              Tab(icon: Icon(Icons.list_alt)),
+              Tab(icon: Icon(Icons.check)),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            Icon(Icons.directions_transit, size: 350),
+            Center(child: GridLayout()),
+            Icon(Icons.directions_car, size: 350),
+          ],
+        ),
+      ),
     );
   }
 }
