c4cb930d116e188f924b91b7171c5f9a8d5cbc0e
edd34 <eddine.farid@hotmail.fr>
2022-04-06 20:45:30 +0300

    add id to model AlertTable

--- lib/alert/models/alert_table.dart
+++ lib/alert/models/alert_table.dart
@@ -1,21 +1,24 @@
 class AlertTable {
   AlertTable(
+    this.id,
     this.category,
     this.alertType,
     this.alertSubtype,
   );
 
   AlertTable.fromJson(Map<String, dynamic> _json)
-      : category = _json['category'] as String,
+      : id = _json['id'] as int,
+        category = _json['category'] as String,
         alertType = _json['alert_type'] as String,
         alertSubtype = _json['alert_subtype'] as String;
 
-  String category = '';
-  String alertType = '';
-  String alertSubtype = '';
+  String category;
+  String alertType;
+  String alertSubtype;
+  int id;
 
   @override
   String toString() {
-    return 'AlertTable: {category: $category, alertType: $alertType, alertSubtype: $alertSubtype}';
+    return 'AlertTable: {id: $id, category: $category, alertType: $alertType, alertSubtype: $alertSubtype}';
   }
 }
