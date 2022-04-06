f0ea0a4a7cf73987af85e7a693ee4aea900faaa1
edd34 <eddine.farid@hotmail.fr>
2022-04-06 11:42:21 +0300

    remove unused import

--- lib/alert/providers/position_provider.dart
+++ lib/alert/providers/position_provider.dart
@@ -1,8 +1,5 @@
-import 'dart:html';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
-import 'package:info_traffic_976/alert/models/traffic_alert.dart';
 
 class LocalisationProvider extends ChangeNotifier {
   late GeoPoint _currentPosition;
