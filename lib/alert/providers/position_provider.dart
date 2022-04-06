338c23e8a90512229407c5cf6ab3f233c5b6e08e
edd34 <eddine.farid@hotmail.fr>
2022-04-06 10:43:14 +0300

    add position provider

---
+++ lib/alert/providers/position_provider.dart
@@ -0,0 +1,13 @@
+import 'dart:html';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+import 'package:info_traffic_976/alert/models/traffic_alert.dart';
+
+class LocalisationProvider extends ChangeNotifier {
+  late GeoPoint _currentPosition;
+
+  GeoPoint get currentPosition => _currentPosition;
+  void setShouldRefresh({required GeoPoint value}) =>
+      {_currentPosition = value};
+}
