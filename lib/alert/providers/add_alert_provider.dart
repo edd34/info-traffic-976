d735aa36a7e45ff16d3236c4c270877ea7256bcb
edd34 <eddine.farid@hotmail.fr>
2022-04-08 20:54:30 +0300

    add attributes and method to add_alert_provider

--- lib/alert/providers/add_alert_provider.dart
+++ lib/alert/providers/add_alert_provider.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+import 'package:info_traffic_976/alert/models/alert_table.dart';
 
 class AddAlertProvider extends ChangeNotifier {
   late GeoPoint _currentPosition;
+  late AlertTable _selectedAlert;
+  bool _completed = false;
+  int _currentTabIndex = 0;
 
+  int get currentTabIndex => _currentTabIndex;
+  AlertTable get selectedAlert => _selectedAlert;
+  bool get completed => _completed;
   GeoPoint get currentPosition => _currentPosition;
+
+  void resetCurrentTabIndex() => {_currentTabIndex = 0};
+  void goNextTabIndex() => {_currentTabIndex++};
+  void goPreviousTabIndex() => {_currentTabIndex--};
+
+  void setCompleted() => {_completed = true};
+  void resetCompleted() => {_completed = false};
+  void setCurrentAlert({required AlertTable value}) => {_selectedAlert = value};
   void setCurrentPosition({required GeoPoint value}) =>
       {_currentPosition = value};
 }
